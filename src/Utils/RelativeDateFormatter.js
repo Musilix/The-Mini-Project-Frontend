92db771ef4a6dfe724d6899a7ed9e3a40d819ca6
Kareem Shehab <kshehab1@student.gsu.edu>
2022-10-10 01:59:18 -0700

    Switchin over to luxon for date management

--- src/Utils/RelativeDateFormatter.js
+++ src/Utils/RelativeDateFormatter.js
@@ -1,44 +1,59 @@
-export function RelativeDateFormatter(firstDate, secondDate = Date.now()) {
-  let firstDateAdjusted = secondDate;
-  if (firstDate) {
-    firstDateAdjusted = new Date(firstDate);
-    firstDateAdjusted.setMinutes(
-      firstDateAdjusted.getMinutes() - firstDateAdjusted.getTimezoneOffset()
-    );
-  }
+import { DateTime } from "luxon";
+// I should probably use a library for this, BUT I REFUSE
+export function RelativeDateFormatter(firstDate, secondDate = DateTime.now()) {
+  let adjustedDate = firstDate
+    ? DateTime.fromISO(firstDate).toLocal()
+    : secondDate; //get out date in a format luxor can use, then convert to users locale for calcs
+  const deltas = timeDifference(adjustedDate, secondDate);
 
-  const deltaD = timeDifference(firstDateAdjusted, secondDate);
-  //TODO: add months and years
-
-  return convertTimeToReadable(deltaD);
+  return convertTimeToReadable(deltas);
 }
 
-const convertTimeToReadable = (time) => {
-  let secs = Math.round(time / 1000);
-  let mins = Math.round(time / (1000 * 60));
-  let hrs = Math.round(time / (1000 * 60 * 60));
-  let days = Math.round(time / (1000 * 60 * 60 * 24));
+const convertTimeToReadable = (deltas) => {
+  const secs = Math.floor(deltas.seconds);
+  const mins = Math.floor(deltas.minutes);
+  const hrs = Math.floor(deltas.hours);
+  const days = Math.floor(deltas.days);
+  const months = Math.floor(deltas.months);
+  const years = Math.floor(deltas.years);
 
-  // loop through time elements
-  // check if secs is readable (1-59), mins is readable (1-59) or days is visible (1-infinity)
-  if (secs > 0 && secs < 59) {
-    return `${secs} second${secs > 1 ? "s" : ""} ago`;
-  } else if (mins > 0 && mins < 59) {
-    return `${mins} minute${mins > 1 ? "s" : ""}  ago`;
-  } else if (hrs > 0 && hrs < 59) {
-    return `${hrs} hour${hrs > 1 ? "s" : ""}  ago`;
-  } else if (days > 0) {
-    return `${days} day${days > 1 ? "s" : ""}  ago`;
-  } else {
-    return `extremely recent`; //fail safe if for some reason an undefined/NaN val gets created... somehow this was happening at one point, but seemingly resolved itself. Weird. We'll keep a look out in the future
+  // brute force for now, go top down, largest to smallest time measurement to find which we should use for post/edit times
+  if (years) {
+    return `${years} year${years > 1 ? "s" : ""}  ago`;
   }
+
+  if (months) {
+    return `${months} month${months > 1 ? "s" : ""}  ago`;
+  }
+
+  if (days) {
+    return `${days} day${days > 1 ? "s" : ""}  ago`;
+  }
+
+  if (hrs) {
+    return `${hrs} hour${hrs > 1 ? "s" : ""}  ago`;
+  }
+
+  if (mins) {
+    return `${mins} minute${mins > 1 ? "s" : ""}  ago`;
+  }
+
+  if (secs) {
+    return `${secs} second${secs > 1 ? "s" : ""} ago`;
+  }
+
+  return `extremely recent`; //fail safe if for some reason an undefined/NaN val gets created... somehow this was happening at one point, but seemingly resolved itself. Weird. We'll keep a look out in the future
 };
 
 const timeDifference = (d1, d2) => {
-  const postDate = new Date(d1);
-  const now = new Date(d2);
+  const deltas = d2.diff(d1, [
+    "years",
+    "months",
+    "days",
+    "hours",
+    "minutes",
+    "seconds",
+  ]);
 
-  const deltaD = Math.abs(now - postDate);
-
-  return deltaD;
+  return deltas.values;
 };
